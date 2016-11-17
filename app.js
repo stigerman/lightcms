283b3d856533f3c05376492bdc60ed89b19f2860
stigerman <jarred.stiger@gmail.com>
2016-11-17 07:12:09 +0000

    fixes5

--- app.js
+++ app.js
@@ -35,9 +35,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(function(req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/public/index.html');
 });
 
 
